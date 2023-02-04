05754e8d976259a7fd204ccadc30309053b17c69
Shish <shish@shishnet.org>
2023-02-04 12:51:14 +0000

    format

--- tests/HTMLElementTest.php
+++ tests/HTMLElementTest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 require "vendor/autoload.php";
 
@@ -184,4 +186,3 @@ class HTMLElementTest extends \PHPUnit\Framework\TestCase
         );
     }
 }
-
