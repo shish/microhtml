7cbed6e73454f30fe96592d44cbce2819a2d7702
Shish <shish@shishnet.org>
2023-02-04 12:56:54 +0000

    fix test

--- tests/CodeTest.php
+++ tests/CodeTest.php
@@ -16,7 +16,8 @@ class CodeTest extends \PHPUnit\Framework\TestCase
             if (preg_match("/function ([A-Z][^(]*)/", $line, $matches)) {
                 $fun = $matches[1];
                 $tag = $exceptions[$fun] ?? strtolower($fun);
-                $this->assertStringContainsString("\"$tag\"", $line);
+                // Call eg \MicroHTML\SECTION() and check that it contains "section"
+                $this->assertStringContainsString($tag, (string)"\MicroHTML\\$fun"());
             }
         }
     }
