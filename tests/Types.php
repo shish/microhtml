b86478ddc3bc20208dd87878a2dbf2243aeb3265
Shish <shish@shishnet.org>
2026-07-02 02:08:48 +0100

    bumps

--- tests/Types.php
+++ tests/Types.php
@@ -4,15 +4,17 @@
 // the allowed lines and reject the disallowed lines
 declare(strict_types=1);
 
-use function MicroHTML\{P, IMG};
+use function MicroHTML\{BODY, P, IMG};
 
-P(["class" => "foo"]); // attributes allowed in first argument
-P("words", ["property" => "array"]); // @phpstan-ignore-line - attributes not allowed in any other argument
-P(["foo", "bar", "baz"]); // @phpstan-ignore-line - attribute arrays must be string=>string
-P(P(), P()); // multiple children allowed
-P([P(), P()]); // @phpstan-ignore-line - no arrays-of-children allowed
+print(BODY(
+    P(["class" => "foo"]), // attributes allowed in first argument
+    P("words", ["property" => "array"]), // @phpstan-ignore-line - attributes not allowed in any other argument
+    P(["foo", "bar", "baz"]), // @phpstan-ignore-line - attribute arrays must be string=>string
+    P(P(), P()), // multiple children allowed
+    P([P(), P()]), // @phpstan-ignore-line - no arrays-of-children allowed
 
-// void tags should have no children
-IMG(["src" => "foo.jpg"]); // attributes allowed
-IMG(["foo", "bar"]); // @phpstan-ignore-line - attribute arrays must be string=>string
-IMG(P()); // @phpstan-ignore-line - children not allowed
+    // void tags should have no children
+    IMG(["src" => "foo.jpg"]), // attributes allowed
+    IMG(["foo", "bar"]), // @phpstan-ignore-line - attribute arrays must be string=>string
+    IMG(P()), // @phpstan-ignore-line - children not allowed
+));
